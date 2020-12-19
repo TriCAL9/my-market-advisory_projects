693a520026c541861d46c34c0d8aa6e45f5bf264
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2020-12-19 12:57:17 -0500

    now extends the new BaseMarket, mutator methods are now public and the types have been migrated to Date for the opening and closing trade
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-jpa/src/main/java/com/unknown/jpa/model/TradeReceipt.java
+++ market-recommender-jpa/src/main/java/com/unknown/jpa/model/TradeReceipt.java
@@ -6,21 +6,22 @@
 package com.unknown.jpa.model;
 
 import com.unknown.commons.base.data.BaseTradeReceipt;
-import lombok.*;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.sql.Date;
 
 /**
  * @author CalAF
  */
 @Data
-@Getter(AccessLevel.PRIVATE)
-@Setter(AccessLevel.PRIVATE)
 public class TradeReceipt implements BaseTradeReceipt {
     @NonNull
     String Trade_Owner;
     String Trade_Price;
     String Trade_Type;
-    String Trade_Opening_Date;
-    String Trade_Closing_Date;
+    Date Trade_Opening_Date;
+    Date Trade_Closing_Date;
     String Trade_Strategy_Name;
     String Trade_Transaction_Symbol;
 }
