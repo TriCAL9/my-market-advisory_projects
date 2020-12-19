b3f3a3e7dd200830d58f57314ae9fec746fd5776
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2020-12-19 06:16:38 -0500

    interface now extends BaseMarket parameterize with the new generic type BaseMember
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-commons/src/main/java/com/unknown/commons/base/repo/BaseMarketRepo.java
+++ market-recommender-commons/src/main/java/com/unknown/commons/base/repo/BaseMarketRepo.java
@@ -1,13 +1,15 @@
 package com.unknown.commons.base.repo;
 
 import com.unknown.commons.base.data.BaseMarket;
+import com.unknown.commons.base.data.BaseMember;
 import com.unknown.commons.properties.AssetType;
 import org.springframework.data.repository.CrudRepository;
 
 import java.io.Serializable;
 import java.util.List;
 
-public interface BaseMarketRepo<T extends BaseMarket<ID>, ID extends Serializable> extends CrudRepository<T, ID> {
+public interface BaseMarketRepo<T extends BaseMarket<BaseMember<Integer>, ID>, ID extends Serializable>
+        extends CrudRepository<T, ID> {
 
     List<T> readDistinctMarketByMarket_Asset_Type(AssetType assetType);
 
