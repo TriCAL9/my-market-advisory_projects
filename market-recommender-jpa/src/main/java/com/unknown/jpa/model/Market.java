6e453fed642177715c927621748768c255b5ded5
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2020-12-19 12:54:17 -0500

    now extends the new BaseMarket
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-jpa/src/main/java/com/unknown/jpa/model/Market.java
+++ market-recommender-jpa/src/main/java/com/unknown/jpa/model/Market.java
@@ -6,6 +6,7 @@
 package com.unknown.jpa.model;
 
 import com.unknown.commons.base.data.BaseMarket;
+import com.unknown.commons.base.data.BaseMember;
 import com.unknown.commons.implemented.Asset;
 import lombok.Data;
 import lombok.NonNull;
@@ -17,8 +18,7 @@ import java.sql.Date;
  * @author CalAF
  */
 @Data
-public class Market implements BaseMarket<Date> {
-
+public class Market implements BaseMarket<BaseMember<Integer>, Date> {
     @Id
     @NonNull
     Date Market_Transaction_Date;
@@ -27,6 +27,4 @@ public class Market implements BaseMarket<Date> {
     String Market_Portfolio_Name;
     String Market_Technical_Condition;
     Asset Market_Asset_Type;
-
-
 }
