52976dcd46c29351df7ad99c5f0ab3b597b08ce8
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2021-03-14 05:58:02 -0400

    removed @BeforeMethod
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-test/src/main/java/api/recomm/test/BaseUserProfile1RepoTest.java
+++ market-recommender-test/src/main/java/api/recomm/test/BaseUserProfile1RepoTest.java
@@ -5,43 +5,33 @@ import com.marketav.commons.base.repo.BaseUserProfile1Repo;
 import com.marketav.commons.implemented.WildCardConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Optional;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
-@Test(groups = "testUserProfile")
+@Test(groups = "testProfile")
 public abstract class BaseUserProfile1RepoTest<U extends BaseUserProfile1<ID>, ID> extends AbstractTestNGSpringContextTests {
     @Autowired
     BaseUserProfile1Repo<U, ID> userProfile1Repo;
-
     @Autowired
     WildCardConverter converter;
 
     protected abstract U createUserProfile1(String profileName, String profileEmail, String pass);
 
-
-    @BeforeMethod(dependsOnGroups = {"testUserProfile2", "testMember"})
-    public void clearDatabase() {
-        if (userProfile1Repo.count() > 0) {
-            userProfile1Repo.deleteAll();
-        }
-    }
-
-    @Test(groups = "testUserProfile")
+    @Test
     public void testOperations() {
-        assertEquals(userProfile1Repo.count(), 0);
         U user1 = createUserProfile1("Owner", "cartworld@shopping.com", "xxxxx");
         U user2 = createUserProfile1("member", "lookatusbunch@outlook.com", "xxxxx");
         userProfile1Repo.save(user1);
-        assertEquals(userProfile1Repo.count(), 1);
+        assertEquals(userProfile1Repo.count(), 2);
         Optional<U> user = userProfile1Repo.findByProfileEmail(user1.getProfileEmail());
         assertTrue(user.isPresent());
         assertEquals(user.get().getProfileName(), user1.getProfileName());
         userProfile1Repo.save(user2);
         Optional<U> userAgain = userProfile1Repo.findByProfileEmail(user2.getProfileEmail());
+        assertTrue(userAgain.isPresent());
         assertEquals(userAgain.get().getProfileName(), user2.getProfileName());
         assertEquals(userProfile1Repo.count(), 2);
     }
--- market-recommender-test/src/main/java/api/recomm/test/BaseUserProfile2RepoTest.java
+++ market-recommender-test/src/main/java/api/recomm/test/BaseUserProfile2RepoTest.java
@@ -4,7 +4,6 @@ import com.marketav.commons.base.data.BaseUserProfile2;
 import com.marketav.commons.base.repo.BaseUserProfile2Repo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.Serializable;
@@ -16,18 +15,14 @@ public abstract class BaseUserProfile2RepoTest<U extends BaseUserProfile2, ID ex
     @Autowired
     BaseUserProfile2Repo<U, ID> userProfile2Repo;
 
-    protected abstract BaseUserProfile2 createUserProfile2(String email, LocalDate timeIn, LocalDate timeOut, boolean signedInStatus);
+    protected abstract U createUserProfile2(String email, LocalDate timeIn, LocalDate timeOut, boolean signedInStatus);
 
-    @BeforeMethod
-    public void clearDatabase() {
-        userProfile2Repo.deleteAll();
-    }
-
-    @Test
+    @Test(dependsOnGroups = "testProfile")
     public void testOperation() {
         assertEquals(userProfile2Repo.count(), 0);
-        BaseUserProfile2 userStamp = createUserProfile2("cartworld@shopping.com", LocalDate.now(), null, true);
-        BaseUserProfile2 userStamp1 = createUserProfile2("lookatusbunch@outlook.com", LocalDate.now(), null, true);
+        U userStamp = createUserProfile2("cartworld@shopping.com", LocalDate.of(LocalDate.EPOCH.getYear(), LocalDate.EPOCH.getMonth(), LocalDate.EPOCH.getDayOfMonth()), LocalDate.now().plusDays(1), true);
+        U userStamp1 = createUserProfile2("lookatusbunch@outlook.com", LocalDate.of(LocalDate.EPOCH.getYear(), LocalDate.EPOCH.getMonth(), LocalDate.EPOCH.getDayOfMonth()), LocalDate.now().plusDays(3), true);
+        userProfile2Repo.save(userStamp);
     }
 
 }
