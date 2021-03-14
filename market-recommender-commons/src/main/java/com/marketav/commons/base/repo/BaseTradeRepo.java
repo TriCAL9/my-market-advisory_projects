e870d7dfb05e5c2d0eeb80d8406d264fb4382590
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2021-03-14 05:56:58 -0400

    initial test for Trade entity.
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-commons/src/main/java/com/marketav/commons/base/repo/BaseTradeRepo.java
+++ market-recommender-commons/src/main/java/com/marketav/commons/base/repo/BaseTradeRepo.java
@@ -3,8 +3,6 @@ package com.marketav.commons.base.repo;
 import com.marketav.commons.base.data.BaseTrade;
 import org.springframework.data.repository.CrudRepository;
 
-import java.io.Serializable;
-
-public interface BaseTradeRepo<T extends BaseTrade, ID extends Serializable>
+public interface BaseTradeRepo<T extends BaseTrade, ID>
         extends CrudRepository<T, ID> {
 }
--- market-recommender-test/src/main/java/api/recomm/test/BaseTradeRepoTest.java
+++ market-recommender-test/src/main/java/api/recomm/test/BaseTradeRepoTest.java
@@ -1,7 +1,39 @@
 package api.recomm.test;
 
+import com.marketav.commons.base.data.BaseTrade;
+import com.marketav.commons.base.repo.BaseTradeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class BaseTradeRepoTest extends AbstractTestNGSpringContextTests {
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+@Test(groups = "testTrade", dependsOnGroups = {"testMemberOperation"})
+public abstract class BaseTradeRepoTest<T extends BaseTrade, ID> extends AbstractTestNGSpringContextTests {
+    @Autowired
+    BaseTradeRepo<T, ID> baseTradeRepo;
+
+    protected abstract T createTrade(String profileEmail, Integer memberId, LocalDate marketTransactionDate,
+                                     Integer marketId, BigDecimal tradePrice, LocalDate tradeClosingDate,
+                                     String tradeType, LocalDate tradeOpeningDate, String memberFirstName, String memberLastName);
+
+    @Test
+    public void testOperation() {
+        T initialTrade = createTrade("cartworld@shopping.com", 1, LocalDate.now(), 1,
+                BigDecimal.valueOf(12.50), LocalDate.ofYearDay(LocalDate.now().getYear(),
+                        LocalDate.now().getDayOfYear()), "BUY", LocalDate.now(),
+                "Suck", "Dick");
+        baseTradeRepo.save(initialTrade);
+
+        Assert.assertEquals(baseTradeRepo.count(), 1);
+
+        T secondTrade = createTrade("lookatusbunch@outlook.com", 2, LocalDate.now().plusDays(1), 2,
+                BigDecimal.valueOf(11.50), LocalDate.now().plusDays(2), "BUY", LocalDate.now().plusDays(1),
+                "Little", "Bitch");
+        baseTradeRepo.save(secondTrade);
+
+        Assert.assertEquals(baseTradeRepo.count(), 2);
+    }
 }
