f6b78acd45a39486700cc2eac9181f2a92379165
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2021-03-14 06:06:10 -0400

    implemented the spring standard for using properties instead of instance fields and refactored the class
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- my-market-advisory/src/main/java/api/my_market_advisor/controller/MarketController.java
+++ my-market-advisory/src/main/java/api/my_market_advisor/controller/MarketController.java
@@ -1,9 +1,9 @@
 package api.my_market_advisor.controller;
 
+import api.my_market_advisor.components.IEXCloudComponent;
 import api.my_market_advisor.model.Logo;
 import api.my_market_advisor.model.News;
 import api.my_market_advisor.model.StockProfile;
-import api.my_market_advisor.service.MarketControllerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -19,42 +19,44 @@ import java.util.List;
 import java.util.Objects;
 
 @RestController
-public class MarketController extends RestTemplate {
+class MarketController extends RestTemplate {
     @Autowired
-    MarketControllerProperties env;
+    private IEXCloudComponent iexCloudComponent;
     @Autowired
-    RestTemplateBuilder builder;
+    private RestTemplateBuilder builder;
 
     @GetMapping(value = "{symbol}/{last}")
     public ModelAndView feedNews(@PathVariable(name = "symbol") String symbol
             , @PathVariable(name = "last") String last) {
         final ModelAndView news = new ModelAndView("news");
         final var size = Integer.parseInt(last);
-        List<News> latestNews = new ArrayList<>();
-        for (News.Builder builders : buildMarketData(symbol, size, News.Builder[].class)) {
-            final var news1 = builders.build();
-            latestNews.add(builders.addSummary(news1.getSummary()).build());
-        }
-        news.addObject("stockProfile", buildMarketData(symbol, StockProfile.Builder.class).addSymbol(symbol).build());
-        news.addObject("latestNews", latestNews);
+        news.addObject("stockProfile", buildMarketData(symbol));
+        news.addObject("latestNews", buildMarketData(symbol, size));
         news.addObject("logo", getLogo(symbol));
         return news;
     }
 
-    public News.Builder[] buildMarketData(String symbol, int size, Class<? extends News.Builder[]> news) {
-        return Objects.requireNonNull(restTemplate(builder).getForObject(initializeURI(symbol, News.getPARAMETER(), size),
-                news), "A null object is not accepted. "
-                + "It is recommended that you check the structure of the Json object before continuing.");
+    List<News> buildMarketData(String symbol, int size) {
+        News.Builder[] builders = Objects.requireNonNull(restTemplate(builder).getForObject(initializeURI(symbol,
+                News.getPARAMETER(), size), (Class<? extends News.Builder[]>) News.Builder[].class),
+                "A null object is not accepted. It is recommended that you check the structure of the Json" +
+                        " object before continuing.");
+        List<News> latestNews = new ArrayList<>();
+        for (News.Builder builder : builders) {
+            latestNews.add(builder.build());
+        }
+        return latestNews;
     }
 
-    public StockProfile.Builder buildMarketData(String symbol, Class<? extends StockProfile.Builder> stockProfile) {
-        return Objects.requireNonNull(restTemplate(builder).getForObject(initializeURI(symbol, StockProfile.getPARAMETER()),
-                stockProfile), "A null object is not accepted. "
+    StockProfile buildMarketData(String symbol) {
+        StockProfile.Builder builder1 = Objects.requireNonNull(restTemplate(builder).getForObject(initializeURI(symbol, StockProfile.getPARAMETER()),
+                (Class<? extends StockProfile.Builder>) StockProfile.Builder.class), "A null object is not accepted. "
                 + "It is recommended that you check the structure of the Json object before continuing.");
+        return builder1.addSymbol(symbol).build();
     }
 
-    public Logo getLogo(String symbol) {
-        var temp = env.getProperties().getLogo().expand(symbol, env.getProperties().getIex_cloud_key());
+    Logo getLogo(String symbol) {
+        var temp = iexCloudComponent.getProperties().getLogo().expand(symbol, iexCloudComponent.getProperties().getIex_cloud_key());
         return Objects.requireNonNull(restTemplate(builder).getForObject(temp.toString(), Logo.class), "Logo not found");
     }
 
@@ -63,12 +65,15 @@ public class MarketController extends RestTemplate {
         return builder.build();
     }
 
-    private URI initializeURI(String symbol, String filter, int last) {
-        return env.getProperties().getUrl().expand(symbol, filter, env.getProperties().getLast().expand(last), env.getProperties().getIex_cloud_key());
+    protected URI initializeURI(String symbol, String filter, int last) {
+        return iexCloudComponent.getProperties().getUrl().expand(symbol, filter,
+                iexCloudComponent.getProperties().getLast().expand(last),
+                iexCloudComponent.getProperties().getIex_cloud_key());
     }
 
-    private URI initializeURI(String symbol, String filter) {
-        return env.getProperties().getCompanyUrl().expand(symbol, filter, env.getProperties().getIex_cloud_key());
+    protected URI initializeURI(String symbol, String filter) {
+        return iexCloudComponent.getProperties().getCompanyUrl().expand(symbol,
+                filter, iexCloudComponent.getProperties().getIex_cloud_key());
     }
 
 }
