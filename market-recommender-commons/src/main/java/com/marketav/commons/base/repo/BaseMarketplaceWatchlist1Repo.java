64c4e7c296f6297491a3fd1731090e4171fdb71f
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2021-03-14 05:41:01 -0400

    replaced type with generic type
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-commons/src/main/java/com/marketav/commons/base/repo/BaseMarketplaceWatchlist1Repo.java
+++ market-recommender-commons/src/main/java/com/marketav/commons/base/repo/BaseMarketplaceWatchlist1Repo.java
@@ -8,10 +8,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface BaseMarketplaceWatchlist1Repo<T extends BaseMarketplaceWatchlist1, ID>
-        extends CrudRepository<BaseMarketplaceWatchlist1, ID> {
+        extends CrudRepository<T, ID> {
     @Query("SELECT W.*, M.Market_Portfolio_Name FROM (marketplace_watchlist2 W"
             + " JOIN marketplace_watchlist1 M)"
             + "WHERE W.Market_Id = :marketId")
     List<T> getAllByMarketId(@Param("marketId") int marketID);
-
 }
