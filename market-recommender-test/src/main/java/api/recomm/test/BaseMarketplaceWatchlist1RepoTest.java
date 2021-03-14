042518ff82f70d528cbb3f822006de5557a24fc2
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2021-03-14 05:43:30 -0400

    replaced type with generic type  on class field.
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-test/src/main/java/api/recomm/test/BaseMarketplaceWatchlist1RepoTest.java
+++ market-recommender-test/src/main/java/api/recomm/test/BaseMarketplaceWatchlist1RepoTest.java
@@ -4,24 +4,18 @@ import com.marketav.commons.base.data.BaseMarketplaceWatchlist1;
 import com.marketav.commons.base.repo.BaseMarketplaceWatchlist1Repo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.LocalDate;
 
 import static org.testng.Assert.assertEquals;
-
-public abstract class BaseMarketplaceWatchlist1RepoTest<M extends BaseMarketplaceWatchlist1> extends AbstractTestNGSpringContextTests {
+@Test(groups = "testWatchlist1", dependsOnGroups = "testTrade")
+public abstract class BaseMarketplaceWatchlist1RepoTest<M extends BaseMarketplaceWatchlist1, ID> extends AbstractTestNGSpringContextTests {
 
     @Autowired
-    BaseMarketplaceWatchlist1Repo<M, LocalDate> marketplaceWatchlist1Repo;
+    BaseMarketplaceWatchlist1Repo<M, ID> marketplaceWatchlist1Repo;
 
     protected abstract M createMarketWatchlist(LocalDate transactionDate, Integer marketId, BaseMarketplaceWatchlist1.Asset assetType);
-
-    @BeforeMethod
-    public void clearDatabase() {
-        marketplaceWatchlist1Repo.deleteAll();
-    }
 
     @Test
     public void testOperation() {
