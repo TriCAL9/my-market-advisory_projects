cd36b1c3800bc7e64702a5222beccf1ed276793b
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2021-04-11 14:55:08 -0400

    removed the application annotation and made it a springbootconfiguration class
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-jdbc/src/main/java/com/marketav/MarketAvJdbcApplicationConfig.java
+++ market-recommender-jdbc/src/main/java/com/marketav/MarketAvJdbcApplicationConfig.java
@@ -2,22 +2,18 @@ package com.marketav;
 
 import com.marketav.commons.implemented.WildCardConverter;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootConfiguration
 @EnableJpaRepositories
 @EntityScan
 public class MarketAvJdbcApplicationConfig {
     //TODO: Create a service to delete all users from the database.
-    public static void main(String[] args) {
-        SpringApplication.run(MarketAvJdbcApplicationConfig.class, args);
-    }
 
     @Bean
     WildCardConverter wildCardConverter() {
