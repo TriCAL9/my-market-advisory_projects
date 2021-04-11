e9395be3301357419039627f5b1b9571f07e1c0d
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2021-04-11 14:47:50 -0400

    not much changed just deleted local change made
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-test/src/main/java/api/recomm/test/BaseMarketplaceWatchlist1RepoTest.java
+++ market-recommender-test/src/main/java/api/recomm/test/BaseMarketplaceWatchlist1RepoTest.java
@@ -4,12 +4,13 @@ import com.marketav.commons.base.data.BaseMarketplaceWatchlist1;
 import com.marketav.commons.base.repo.BaseMarketplaceWatchlist1Repo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.LocalDate;
 
 import static org.testng.Assert.assertEquals;
-@Test(groups = "testWatchlist1", dependsOnGroups = "testTrade")
+
 public abstract class BaseMarketplaceWatchlist1RepoTest<M extends BaseMarketplaceWatchlist1, ID> extends AbstractTestNGSpringContextTests {
 
     @Autowired
@@ -17,10 +18,17 @@ public abstract class BaseMarketplaceWatchlist1RepoTest<M extends BaseMarketplac
 
     protected abstract M createMarketWatchlist(LocalDate transactionDate, Integer marketId, BaseMarketplaceWatchlist1.Asset assetType);
 
-    @Test
+    @BeforeMethod
+    protected abstract void clearData();
+
+    public BaseMarketplaceWatchlist1Repo<M, ID> getMarketplaceWatchlist1Repo() {
+        return marketplaceWatchlist1Repo;
+    }
+
+    @Test(alwaysRun = true)
     public void testOperation() {
         M watchlist1 = createMarketWatchlist(LocalDate.now(), 1, BaseMarketplaceWatchlist1.Asset.STOCK);
-        M secondWatchlist11 = createMarketWatchlist(LocalDate.from(LocalDate.now()), 2, BaseMarketplaceWatchlist1.Asset.EFT);
+        M secondWatchlist11 = createMarketWatchlist(LocalDate.now(), 2, BaseMarketplaceWatchlist1.Asset.EFT);
         marketplaceWatchlist1Repo.save(watchlist1);
         marketplaceWatchlist1Repo.save(secondWatchlist11);
         assertEquals(marketplaceWatchlist1Repo.count(), 2);
