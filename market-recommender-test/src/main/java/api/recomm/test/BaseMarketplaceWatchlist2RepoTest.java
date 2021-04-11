9c4499844c111338366a26e8275f93ce6e11c005
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2021-04-11 14:48:59 -0400

    abstracted away the delete method to control the order of deletion
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-test/src/main/java/api/recomm/test/BaseMarketplaceWatchlist2RepoTest.java
+++ market-recommender-test/src/main/java/api/recomm/test/BaseMarketplaceWatchlist2RepoTest.java
@@ -4,6 +4,7 @@ import com.marketav.commons.base.data.BaseMarketplaceWatchlist2;
 import com.marketav.commons.base.repo.BaseMarketplaceWatchlist2Repo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -11,19 +12,26 @@ import java.util.Optional;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
-@Test(groups = "testWatchlist2", dependsOnGroups = "testWatchlist1")
+
 public abstract class BaseMarketplaceWatchlist2RepoTest<M extends BaseMarketplaceWatchlist2<ID>, ID> extends AbstractTestNGSpringContextTests {
     @Autowired
     BaseMarketplaceWatchlist2Repo<M, ID> marketplaceWatchlist2Repo;
 
     protected abstract M createMarketplaceWatchlist2(Integer marketId, String marketPortfolioName);
 
+    @BeforeMethod
+    protected abstract void clearData();
+
+    public BaseMarketplaceWatchlist2Repo<M, ID> getMarketplaceWatchlist2Repo() {
+        return marketplaceWatchlist2Repo;
+    }
+
     @Test
     public void testOperations() {
         M firstWatchlist = createMarketplaceWatchlist2(1, "Long-term Holds");
         M secondWatchlist = createMarketplaceWatchlist2(2, "Short-term Holds");
         marketplaceWatchlist2Repo.save(firstWatchlist);
-        assertEquals(marketplaceWatchlist2Repo.count(), 2);
+        assertEquals(marketplaceWatchlist2Repo.count(), 1);
         Optional<M> portfolio = marketplaceWatchlist2Repo.findByMarketId(1);
         assertTrue(portfolio.isPresent());
         marketplaceWatchlist2Repo.save(secondWatchlist);
