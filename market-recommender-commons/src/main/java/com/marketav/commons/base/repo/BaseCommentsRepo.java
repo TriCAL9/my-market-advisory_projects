7265716e2725bdf81ee8af0497cc5b5f1c7d52d3
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2021-04-11 14:43:52 -0400

    added JPQL Query Annotation to express sql statement instead of named query methods.
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-commons/src/main/java/com/marketav/commons/base/repo/BaseCommentsRepo.java
+++ market-recommender-commons/src/main/java/com/marketav/commons/base/repo/BaseCommentsRepo.java
@@ -1,18 +1,20 @@
 package com.marketav.commons.base.repo;
 
 import com.marketav.commons.base.data.BaseComments;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BaseCommentsRepo<T extends BaseComments<ID>, ID> extends CrudRepository<T, ID> {
+    @Query("select c from Comments where c.Member_Comments = :comment")
+    Optional<T> findByMemberComments(@Param("comment") ID comment);
 
-    Optional<T> findByMemberComments(ID comment);
+    @Query("select distinct u from Comments where u.Profile_Email = :email")
+    List<T> findDistinctCommentsAllByProfileEmail(@Param("email") String profileEmail);
 
-    List<T> findAllByMemberId(Integer id);
-
-    List<T> findDistinctCommentsAllByProfileEmail(String profileEmail);
-
-    Optional<T> findByMemberCommentsAndMemberIdAndProfileEmail(ID memberComment, int i, ID email);
+    @Query("select c from Comments where c.Member_Id = :member_id")
+    List<T> findAllCommentsByMemberId(@Param("member_id") int id);
 }
