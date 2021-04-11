796ac6179983ef38656a0afca6bd43d592f302d9
Calvin Francis <4331539+TriCAL9@users.noreply.github.com>
2021-04-11 14:45:34 -0400

    rewrote the test so that it is modular because of that it now runs independent of other test cases
    Signed-off-by: Calvin Francis <4331539+TriCAL9@users.noreply.github.com>

--- market-recommender-test/src/main/java/api/recomm/test/BaseCommentsRepoTest.java
+++ market-recommender-test/src/main/java/api/recomm/test/BaseCommentsRepoTest.java
@@ -7,38 +7,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
-import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
-@Test(groups = {"testComments"})
-public abstract class BaseCommentsRepoTest<C extends BaseComments<ID>, ID extends Serializable> extends AbstractTestNGSpringContextTests {
+
+public abstract class BaseCommentsRepoTest<C extends BaseComments<ID>, ID> extends AbstractTestNGSpringContextTests {
 
     @Autowired
     BaseCommentsRepo<C, ID> commentsRepo;
-
     @Autowired
     WildCardConverter converter;
 
+    public BaseCommentsRepo<C, ID> getCommentsRepo() {
+        return commentsRepo;
+    }
+
     protected abstract C createComments(String comments, int memberId, String email);
 
-    @Test(dependsOnGroups = "testMemberQueries")
+    protected abstract void clearData();
+
+    @Test
     public void testOperation() {
         assertEquals(commentsRepo.count(), 0);
         C firstEntity = createComments("Hello! This is taking forever.", 1, "cartworld@shopping.com");
-        C secondEntity = createComments("It worked!", 2, "lookatusbunch@outl" +
+        C secondEntity = createComments("Another closer to finishing.", 1, "cartworld@shopping.com");
+        C thirdEntity = createComments("It worked!", 2, "lookatusbunch@outl" +
                 "ook.com");
         commentsRepo.save(firstEntity);
-        Optional<C> comment = commentsRepo.findByMemberCommentsAndMemberIdAndProfileEmail(firstEntity.getMemberComments(), firstEntity.getMemberId(), firstEntity.getProfileEmail());
-        Optional<C> specificComment = commentsRepo.findByMemberComments(firstEntity.getMemberComments());
-        assertTrue(comment.isPresent());
-        assertEquals(comment.get(), firstEntity);
-        List<C> query = commentsRepo.findAllByMemberId(1);
-        assertEquals(query.size(), 1);
-        assertEquals(query.get(0), secondEntity);
         commentsRepo.save(secondEntity);
+        commentsRepo.save(thirdEntity);
+        List<C> comments = commentsRepo.findAllCommentsByMemberId(1);
+        assertEquals(comments.size(), 2);
+        Optional<C> specificComment = commentsRepo.findByMemberComments(secondEntity.getMemberComments());
+        assertTrue(specificComment.isPresent());
+        assertEquals(specificComment.get(), secondEntity);
+        List<C> query = commentsRepo.findAllCommentsByMemberId(2);
+        assertEquals(query.size(), 1);
+        assertEquals(query.get(0), thirdEntity);
+        commentsRepo.save(thirdEntity);
         query = commentsRepo.findDistinctCommentsAllByProfileEmail(converter.convertToWildCard("loo"));
         assertEquals(query.size(), 0);
     }
