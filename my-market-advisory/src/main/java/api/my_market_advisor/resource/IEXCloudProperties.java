f02aab8db4391dab716294d77591ab2837d519f1
TriCAL9 <calafrancis@outlook.com>
2021-09-08 00:43:39 +0000

    now properties file can expand uriTemplate for historical data request.

--- my-market-advisory/src/main/java/api/my_market_advisor/resource/IEXCloudProperties.java
+++ my-market-advisory/src/main/java/api/my_market_advisor/resource/IEXCloudProperties.java
@@ -1,16 +1,19 @@
 package api.my_market_advisor.resource;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.util.UriTemplate;
+
+import lombok.Getter;
+import lombok.Setter;
+
 
 @ConfigurationProperties("app")
 @Getter
 @Setter
-public class IEXCloudProperties {
+public final class IEXCloudProperties {
     private UriTemplate url;
     private UriTemplate companyUrl;
+    private UriTemplate historicalUrl;
     private UriTemplate last;
     private UriTemplate logo;
     private String iex_cloud_key;
