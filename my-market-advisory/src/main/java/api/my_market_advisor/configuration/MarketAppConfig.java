0dc3d0fb5b246568d2439e1e10e4d17dfa2faa95
Calvin Francis <TriCAL9@users.noreply.github.com>
2021-11-15 03:32:31 -0500

    transfer test

--- my-market-advisory/src/main/java/api/my_market_advisor/configuration/MarketAppConfig.java
+++ my-market-advisory/src/main/java/api/my_market_advisor/configuration/MarketAppConfig.java
@@ -17,46 +17,11 @@ import api.my_market_advisor.resource.LogoURIHandler;
 import api.my_market_advisor.resource.NewsURIHandler;
 
 @Configuration
-@ComponentScan("api.my_market_advisor.resource")
 public class MarketAppConfig {
-   /*
-    @Bean
-    @Override
-    public HistoricalStockPriceRequestHandler geHistoricalStockPriceRequestHandler(@Autowired RestTemplate restTemplate,
-           @Autowired HistoricalStockPriceURIHandler historicalStockPriceURIHandler) {
-        // TODO Auto-generated method stub
-        return new HistoricalStockPriceRequestHandler(historicalStockPriceURIHandler, restTemplate) ;
-    }
-    @Bean
-    @Override
-    public NewsRequestHandler getNewsMarketDataRequestHandler(@Autowired RestTemplate restTemplate, @Autowired NewsURIHandler newsURIHandler
-    ,@Autowired LogoURIHandler logoURIHandler, @Autowired CompanyURIHandler companyURIHandler) {
-        // TODO Auto-generated method stub
-        return new NewsRequestHandler(restTemplate, companyURIHandler, newsURIHandler, logoURIHandler) ;
-    }
-    @Bean
-    RestTemplate restTemplate(RestTemplateBuilder builder){
-         return builder.build();
-    }
-    @Bean
-    public String symbol(String symbol) {
-        return "ab";
-    }
-    @Bean
-    NewsURIHandler newsURIHandler(String last, String symbol){
-        return new NewsURIHandler(last, symbol);
-    }
-    @Bean
-    LogoURIHandler logoURIHandler(String symbol) {
-        return new LogoURIHandler(symbol);
-    }
-    @Bean
-    CompanyURIHandler companyURIHandler(String symbol){
-        return new CompanyURIHandler(symbol);
-    }
-    */
-    @Bean 
-    IEXCloudComponent iexCloudComponent() {
-        return new IEXCloudComponent();
+    private Map<URI, String> uriMapsToSymbol;
+    
+    @Autowired
+    public MarketAppConfig(List<URIHandler> uriList, Sting symbol) {
+        uriMapsToSymbol = uriList.stream().collect(Collectors.toMap(URIHandler::getURI, symbol);
     }
 }
